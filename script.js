10910691bb384d2e231cb3636a48f17906811c82
akshatg2902 <satyamgupta2902@gmail.com>
2020-12-21 00:11:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 function GameManager(size, InputManager, Actuator) {
-  this.size         = size;  
+  this.size = size;  
   this.inputManager = new InputManager;
-  this.actuator     = new Actuator;
-  this.startTiles   = 2;
+  this.actuator = new Actuator;
+  this.startTiles = 2;
   this.inputManager.on("move", this.move.bind(this));
   this.inputManager.on("restart", this.restart.bind(this));
   this.setup();
@@ -31,10 +31,10 @@ GameManager.prototype.restart = function () {
 };
 
 GameManager.prototype.setup = function () {
-  this.grid         = new Grid(this.size);
-  this.score        = 0;
-  this.over         = false;
-  this.won          = false;
+  this.grid = new Grid(this.size);
+  this.score = 0;
+  this.over = false;
+  this.won = false;
   this.addStartTiles();
   this.actuate();
 };
@@ -49,7 +49,6 @@ GameManager.prototype.addRandomTile = function () {
   if (this.grid.cellsAvailable()) {
     var value = Math.random() < 0.9 ? 2 : 4;
     var tile = new Tile(this.grid.randomAvailableCell(), value);
-
     this.grid.insertTile(tile);
   }
 };
@@ -57,8 +56,8 @@ GameManager.prototype.addRandomTile = function () {
 GameManager.prototype.actuate = function () {
   this.actuator.actuate(this.grid, {
     score: this.score,
-    over:  this.over,
-    won:   this.won
+    over: this.over,
+    won: this.won
   });
 };
 
@@ -80,59 +79,49 @@ GameManager.prototype.moveTile = function (tile, cell) {
 GameManager.prototype.move = function (direction) {
   var self = this;
   if (this.over || this.won) return;
-
   var cell, tile;
   var vector     = this.getVector(direction);
   var traversals = this.buildTraversals(vector);
   var moved      = false;
   this.prepareTiles();
-
   traversals.x.forEach(function (x) {
     traversals.y.forEach(function (y) {
       cell = { x: x, y: y };
       tile = self.grid.cellContent(cell);
-
       if (tile) {
         var positions = self.findFarthestPosition(cell, vector);
         var next      = self.grid.cellContent(positions.next);
-
         if (next && next.value === tile.value && !next.mergedFrom) {
           var merged = new Tile(positions.next, tile.value * 2);
           merged.mergedFrom = [tile, next];
-
           self.grid.insertTile(merged);
           self.grid.removeTile(tile);
           tile.updatePosition(positions.next);
           self.score += merged.value;
-
           if (merged.value === 2048) self.won = true;
         } else {
           self.moveTile(tile, positions.farthest);
         }
-
         if (!self.positionsEqual(cell, tile)) {
           moved = true;
         }
       }
     });
   });
-
   if (moved) {
     this.addRandomTile();
-
     if (!this.movesAvailable()) {
       this.over = true;
     }
-
     this.actuate();
   }
 };
 
 GameManager.prototype.getVector = function (direction) {
   var map = {
-    0: { x: 0,  y: -1 }, 
-    1: { x: 1,  y: 0 },  
-    2: { x: 0,  y: 1 },  
+    0: { x: 0, y: -1 }, 
+    1: { x: 1, y: 0 },  
+    2: { x: 0, y: 1 },  
     3: { x: -1, y: 0 }   
   };
   return map[direction];
@@ -140,12 +129,10 @@ GameManager.prototype.getVector = function (direction) {
 
 GameManager.prototype.buildTraversals = function (vector) {
   var traversals = { x: [], y: [] };
-
   for (var pos = 0; pos < this.size; pos++) {
     traversals.x.push(pos);
     traversals.y.push(pos);
   }
-
   if (vector.x === 1) traversals.x = traversals.x.reverse();
   if (vector.y === 1) traversals.y = traversals.y.reverse();
 
@@ -156,9 +143,8 @@ GameManager.prototype.findFarthestPosition = function (cell, vector) {
   var previous;
   do {
     previous = cell;
-    cell     = { x: previous.x + vector.x, y: previous.y + vector.y };
-  } while (this.grid.withinBounds(cell) &&
-           this.grid.cellAvailable(cell));
+    cell = { x: previous.x + vector.x, y: previous.y + vector.y };
+  } while (this.grid.withinBounds(cell) && this.grid.cellAvailable(cell));
 
   return {
     farthest: previous,
@@ -176,7 +162,6 @@ GameManager.prototype.tileMatchesAvailable = function () {
   for (var x = 0; x < this.size; x++) {
     for (var y = 0; y < this.size; y++) {
       tile = this.grid.cellContent({ x: x, y: y });
-
       if (tile) {
         for (var direction = 0; direction < 4; direction++) {
           var vector = self.getVector(direction);
@@ -184,7 +169,6 @@ GameManager.prototype.tileMatchesAvailable = function () {
           var other  = self.grid.cellContent(cell);
           if (other) {
           }
-
           if (other && other.value === tile.value) {
             return true; 
           }
@@ -208,7 +192,6 @@ function Grid(size) {
 Grid.prototype.build = function () {
   for (var x = 0; x < this.size; x++) {
     var row = this.cells[x] = [];
-
     for (var y = 0; y < this.size; y++) {
       row.push(null);
     }
@@ -217,7 +200,6 @@ Grid.prototype.build = function () {
 
 Grid.prototype.randomAvailableCell = function () {
   var cells = this.availableCells();
-
   if (cells.length) {
     return cells[Math.floor(Math.random() * cells.length)];
   }
@@ -271,13 +253,12 @@ Grid.prototype.removeTile = function (tile) {
 };
 
 Grid.prototype.withinBounds = function (position) {
-  return position.x >= 0 && position.x < this.size &&
-         position.y >= 0 && position.y < this.size;
+  return position.x >= 0 && position.x < this.size && position.y >= 0 && position.y < this.size;
 };
 
 function HTMLActuator() {
-  this.tileContainer    = document.getElementsByClassName("tile-container")[0];
-  this.scoreContainer   = document.getElementsByClassName("score-container")[0];
+  this.tileContainer = document.getElementsByClassName("tile-container")[0];
+  this.scoreContainer = document.getElementsByClassName("score-container")[0];
   this.messageContainer = document.getElementsByClassName("game-message")[0];
   this.score = 0;
 }
@@ -312,14 +293,12 @@ HTMLActuator.prototype.clearContainer = function (container) {
 
 HTMLActuator.prototype.addTile = function (tile) {
   var self = this;
-  var element   = document.createElement("div");
-  var position  = tile.previousPosition || { x: tile.x, y: tile.y };
+  var element = document.createElement("div");
+  var position = tile.previousPosition || { x: tile.x, y: tile.y };
   positionClass = this.positionClass(position);
   var classes = ["tile", "tile-" + tile.value, positionClass];
   this.applyClasses(element, classes);
-
   element.textContent = tile.value;
-
   if (tile.previousPosition) {
     window.requestAnimationFrame(function () {
       classes[2] = self.positionClass({ x: tile.x, y: tile.y });
@@ -335,7 +314,6 @@ HTMLActuator.prototype.addTile = function (tile) {
     classes.push("tile-new");
     this.applyClasses(element, classes);
   }
-
   this.tileContainer.appendChild(element);
 };
 
@@ -354,12 +332,9 @@ HTMLActuator.prototype.positionClass = function (position) {
 
 HTMLActuator.prototype.updateScore = function (score) {
   this.clearContainer(this.scoreContainer);
-
   var difference = score - this.score;
   this.score = score;
-
   this.scoreContainer.textContent = this.score;
-
   if (difference > 0) {
     var addition = document.createElement("div");
     addition.classList.add("score-addition");
@@ -369,7 +344,7 @@ HTMLActuator.prototype.updateScore = function (score) {
 };
 
 HTMLActuator.prototype.message = function (won) {
-  var type    = won ? "game-won" : "game-over";
+  var type = won ? "game-won" : "game-over";
   var message = won ? "You win!" : "Game over!"
   this.messageContainer.classList.add(type);
   this.messageContainer.getElementsByTagName("p")[0].textContent = message;
@@ -402,7 +377,6 @@ KeyboardInputManager.prototype.emit = function (event, data) {
 
 KeyboardInputManager.prototype.listen = function () {
   var self = this;
-
   var map = {
     38: 0, 
     39: 1, 
@@ -413,28 +387,20 @@ KeyboardInputManager.prototype.listen = function () {
     74: 2,
     72: 3
   };
-
   document.addEventListener("keydown", function (event) {
-    var modifiers = event.altKey || event.ctrlKey || event.metaKey ||
-                    event.shiftKey;
+    var modifiers = event.altKey || event.ctrlKey || event.metaKey || event.shiftKey;
     var mapped = map[event.which];
-
     if (!modifiers) {
       if (mapped !== undefined) {
         event.preventDefault();
         self.emit("move", mapped);
       }
-
       if (event.which === 32) self.restart.bind(self)(event);
     }
   });
-
   var retry = document.getElementsByClassName("retry-button")[0];
   retry.addEventListener("click", this.restart.bind(this));
-
-  var gestures = [Hammer.DIRECTION_UP, Hammer.DIRECTION_RIGHT,
-                  Hammer.DIRECTION_DOWN, Hammer.DIRECTION_LEFT];
-
+  var gestures = [Hammer.DIRECTION_UP, Hammer.DIRECTION_RIGHT,Hammer.DIRECTION_DOWN, Hammer.DIRECTION_LEFT];
   var gameContainer = document.getElementsByClassName("game-container")[0];
   var handler       = Hammer(gameContainer, {
     drag_block_horizontal: true,
@@ -444,7 +410,6 @@ KeyboardInputManager.prototype.listen = function () {
   handler.on("swipe", function (event) {
     event.gesture.preventDefault();
     mapped = gestures.indexOf(event.gesture.direction);
-
     if (mapped !== -1) self.emit("move", mapped);
   });
 };
@@ -455,11 +420,11 @@ KeyboardInputManager.prototype.restart = function (event) {
 };
 
 function Tile(position, value) {
-  this.x                = position.x;
-  this.y                = position.y;
-  this.value            = value || 2;
+  this.x = position.x;
+  this.y = position.y;
+  this.value = value || 2;
   this.previousPosition = null;
-  this.mergedFrom       = null; 
+  this.mergedFrom = null; 
 }
 
 Tile.prototype.savePosition = function () {
